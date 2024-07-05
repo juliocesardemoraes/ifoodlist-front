23ebbb30a5152c12624707bfe7c6ee6acc7a2dd3
julio cesar <jcmcf2012@gmail.com>
2024-07-05 18:39:32 -0300

    fix: more adjustments

--- src/api/foodlist.ts
+++ src/api/foodlist.ts
@@ -1,5 +1,5 @@
 import { redirect } from "next/dist/server/api-utils";
-import { IFoodlist } from "../entities/entities";
+import { IFoodlist, IRestaurant } from "../entities/entities";
 
 export const fetchFoodlists = async (query = ""): Promise<IFoodlist[]> => {
   const requestOptions = {
@@ -15,6 +15,30 @@ export const fetchFoodlists = async (query = ""): Promise<IFoodlist[]> => {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/foodlist`,
+      requestOptions
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("ERROR", error);
+    return error;
+  }
+};
+
+export const fetchRestaurants = async (query = ""): Promise<IRestaurant[]> => {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  const FORCE_EMPTY = false;
+  if (FORCE_EMPTY) return [];
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/restaurant`,
       requestOptions
     );
     const data = await response.json();
@@ -62,6 +86,32 @@ export const addFoodlist = async (Foodlist: IFoodlist): Promise<any> => {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/foodlist`,
+      requestOptions
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("ERROR", error);
+    return error;
+  }
+};
+
+export const addRestaurantToList = async (
+  restaurant: IRestaurant,
+  id: string
+): Promise<any> => {
+  const requestOptions = {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(restaurant),
+  };
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/foodlist?id=${id}`,
       requestOptions
     );
     const data = await response.json();
--- src/app/foodlist/[id]/page.tsx
+++ src/app/foodlist/[id]/page.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { fetchFoodlistById } from "@/api/foodlist";
+import {
+  addRestaurantToList,
+  fetchFoodlistById,
+  fetchRestaurants,
+} from "@/api/foodlist";
 import { IFoodlist, IRestaurant } from "@/entities/entities";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,12 +11,21 @@ import React, { useEffect, useState } from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
   const [data, setData] = useState<IFoodlist | null>(null);
+  const [ids, setIds] = useState<string[] | null>(null);
+  const [dataSuggestions, setDataSuggestions] = useState<IRestaurant[] | null>(
+    null
+  );
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const foodlists = await fetchFoodlistById(params.id);
+        const idsToInsert = [];
+        for (let i = 0; i < foodlists?.restList?.length; i++) {
+          idsToInsert.push(foodlists?.restList[i]?._id);
+        }
+        setIds(idsToInsert);
         setData(foodlists);
       } catch (error) {
         console.error("Failed to fetch foodlists", error);
@@ -21,42 +34,46 @@ export default function Page({ params }: { params: { id: string } }) {
       }
     };
 
+    const getDataSuggestions = async () => {
+      try {
+        const foodlists = await fetchRestaurants(params.id);
+        setDataSuggestions(foodlists);
+      } catch (error) {
+        console.error("Failed to fetch foodlists", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     getData();
+    getDataSuggestions();
   }, [params]);
 
-  useEffect(() => {
-    console.log("DATA", data);
-  }, [data]);
+  const addRestaurant = async (item) => {
+    setData((prev) => {
+      return { ...prev, restList: [...prev.restList, item] };
+    });
+
+    try {
+      await addRestaurantToList(item, params.id);
+    } catch (error) {
+      console.error("Failed to add restaurant", error);
+    }
+
+    const filter = dataSuggestions.filter(
+      (itemSugg) => itemSugg._id != item._id
+    );
+    setDataSuggestions(filter);
+  };
 
   if (isLoading) return <h1>Carregando ...</h1>;
   if (data == null) return <h1>404</h1>;
-  //   if (data.length === 0)
-  //     return (
-  //       <main className="flex flex-col justify-center items-center mt-4">
-  //         <div className="mt-16"></div>
-  //         <Image
-  //           src={"/space.png"}
-  //           quality={100}
-  //           alt="nada aqui"
-  //           width={140}
-  //           height={140}
-  //         />
-  //         <h1 className="mt-8">Nada por aqui ainda</h1>
-  //         <Button
-  //           className="button__primary p-4"
-  //           onClick={redirectToCreate}
-  //           type="button"
-  //         >
-  //           Criar sua primeira foodlist
-  //         </Button>
-  //       </main>
-  //     );
 
   return (
     <>
       <div className="idImage flex justify-center mt-4 mb-4">
         <Image
-          src={data.image}
+          src={data?.image}
           height={100}
           width={100}
           alt={"List image"}
@@ -65,8 +82,8 @@ export default function Page({ params }: { params: { id: string } }) {
       <div className="p-4">
         <h1 className="font-bold">{data?.title}</h1>
         <h4>Criado por: {data.creator}</h4>
-        {data.restList.length > 0 &&
-          data.restList.map((item: IRestaurant) => {
+        {data?.restList.length > 0 &&
+          data?.restList.map((item: IRestaurant) => {
             return (
               <>
                 <div
@@ -75,7 +92,7 @@ export default function Page({ params }: { params: { id: string } }) {
                 >
                   <div className="flex gap-2 items-center">
                     <Image
-                      src={item.image}
+                      src={item?.image}
                       width={50}
                       height={50}
                       alt={"item image"}
@@ -101,6 +118,51 @@ export default function Page({ params }: { params: { id: string } }) {
               </>
             );
           })}
+
+        <div className="mt-12">
+          <h1 className="font-bold">Sugestões</h1>
+        </div>
+        {dataSuggestions?.length > 0 &&
+          dataSuggestions.map((item) => (
+            <>
+              {ids.includes(item._id) ? (
+                <>AH</>
+              ) : (
+                <>
+                  <div
+                    className="item__list flex gap-1 items-center justify-between mt-2"
+                    key={item._id}
+                  >
+                    <div className="flex gap-2 items-center">
+                      <Image
+                        src={item.image}
+                        width={50}
+                        height={50}
+                        alt={"item image"}
+                      />
+                      <div>
+                        <h1>{item.title}</h1>
+                        <div className="flex gap-4">
+                          <p>{item.rank}</p>
+                          <p>{item.type}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="chevron"
+                      target="_blank"
+                      onClick={() => {
+                        addRestaurant(item);
+                      }}
+                    >
+                      <h1 className="font-semibold text-2xl text-red-400">+</h1>
+                    </div>
+                  </div>
+                  <hr className="gray__hr mt-3"></hr>
+                </>
+              )}
+            </>
+          ))}
       </div>
     </>
   );
